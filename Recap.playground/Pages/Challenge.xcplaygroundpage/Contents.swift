65cc1d2f2eee32bba480b4b4ae5d975e71021589
Student <student@macbook-pro-640-reed.local>
2017-04-03 17:55:23 -0400

    Made a lot of progress on the circles

--- Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -25,10 +25,64 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 400)
 
-// Replace this comment and add your code below
+let canvas = Canvas(width: 540, height: 540)
+canvas.drawShapesWithFill = false
+for y in stride(from: 0, through: 520, by: 270){
+    for x in stride(from: 0, through: 520, by: 80){
+        
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 40 + x, centreY: 40 + y, width: 60, height: 60, borderWidth: 10)
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX: 40 + x, centreY: 40 + y, width: 40, height: 40, borderWidth: 10)
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: 40 + x, centreY: 40 + y, width: 20, height: 20, borderWidth: 10)
+        
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 40 + x, centreY: 220 + y, width: 60, height: 60, borderWidth: 10)
+        
+        
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 40 + x, centreY: 220 + y, width: 60, height: 60, borderWidth: 10)
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX: 40 + x, centreY: 220 + y, width: 40, height: 40, borderWidth: 10)
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: 40 + x, centreY: 220 + y, width: 20, height: 20, borderWidth: 10)
+        
+        
+    }
+}
 
+canvas.drawShapesWithFill = false
+for y in stride(from: 0, through: 520, by: 270){
+    for x in stride(from: 0, through: 520, by: 80){
+        
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 60, height: 60, borderWidth: 10)
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 40, height: 40, borderWidth: 10)
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 20, height: 20, borderWidth: 10)
+        
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
+        
+        
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 40, height: 40, borderWidth: 10)
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 20, height: 20, borderWidth: 10)
+        
+        
+    }
+}
+
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawLine(fromX: 0, fromY: 65, toX: 540, toY: 65, lineWidth: 50)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
