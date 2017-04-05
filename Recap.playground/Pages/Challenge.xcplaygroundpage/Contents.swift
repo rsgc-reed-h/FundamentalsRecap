3e6ced4d5815eb702c97fafba7418a337967b821
Student <student@macbook-pro-640-reed.local>
2017-04-05 11:34:38 -0400

    Made my code readable

--- Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -28,6 +28,8 @@ import PlaygroundSupport
 
 let canvas = Canvas(width: 540, height: 540)
 canvas.drawShapesWithFill = false
+
+//Black to yellow circles
 for y in stride(from: 0, through: 520, by: 270){
     for x in stride(from: 0, through: 520, by: 80){
         
@@ -48,73 +50,68 @@ for y in stride(from: 0, through: 520, by: 270){
         canvas.borderColor = Color.yellow
         canvas.drawEllipse(centreX: 40 + x, centreY: 220 + y, width: 20, height: 20, borderWidth: 10)
     }
-}
-
-canvas.drawShapesWithFill = false
-for y in stride(from: 0, through: 540, by: 270){
-    for x in stride(from: 0, through: 540, by: 80){
-        
-        canvas.borderColor = Color.yellow
-        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 60, height: 60, borderWidth: 10)
-        canvas.borderColor = Color.orange
-        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 40, height: 40, borderWidth: 10)
-        canvas.borderColor = Color.black
-        canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 20, height: 20, borderWidth: 10)
-        
-        canvas.borderColor = Color.black
-        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
-        
-        
-        canvas.borderColor = Color.yellow
-        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
-        canvas.borderColor = Color.orange
-        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 40, height: 40, borderWidth: 10)
-        canvas.borderColor = Color.black
-        canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 20, height: 20, borderWidth: 10)
-        
-        
+    
+    //yellow to black circles
+    canvas.drawShapesWithFill = false
+    for y in stride(from: 0, through: 540, by: 270){
+        for x in stride(from: 0, through: 540, by: 80){
+            canvas.borderColor = Color.yellow
+            canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 60, height: 60, borderWidth: 10)
+            canvas.borderColor = Color.orange
+            canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 40, height: 40, borderWidth: 10)
+            canvas.borderColor = Color.black
+            canvas.drawEllipse(centreX: 0 + x, centreY: 90 + y, width: 20, height: 20, borderWidth: 10)
+            canvas.borderColor = Color.black
+            canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
+            canvas.borderColor = Color.yellow
+            canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 60, height: 60, borderWidth: 10)
+            canvas.borderColor = Color.orange
+            canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 40, height: 40, borderWidth: 10)
+            canvas.borderColor = Color.black
+            canvas.drawEllipse(centreX: 0 + x, centreY: 170 + y, width: 20, height: 20, borderWidth: 10)
+        }
+    }
+    //Background that creates aboce circles to become semi circles
+    canvas.borderColor = Color.white
+    canvas.drawRectangle(centreX: 270, centreY: 65, width: 540, height: 0, borderWidth: 55)
+    
+    canvas.borderColor = Color.white
+    canvas.drawRectangle(centreX: 270, centreY: 195, width: 540, height: 0, borderWidth: 55)
+    
+    canvas.borderColor = Color.white
+    canvas.drawRectangle(centreX: 270, centreY: 335, width: 540, height: 0, borderWidth: 55)
+    
+    canvas.borderColor = Color.white
+    canvas.drawRectangle(centreX: 270, centreY: 465, width: 540, height: 0, borderWidth: 55)
+    
+////    //black lines on bottom row
+    for x in stride(from: 10, through: 540, by: 80) {
+        canvas.lineColor = Color.black
+        canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
+    }
+    for x in stride(from: 70, through: 540, by: 80){
+        canvas.lineColor = Color.black
+        canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
+    }
+//    //orange line on bottom row
+    for z in stride(from: 88, to: 260, by: 130){
+    for y in stride(from: 42, through: 260, by: 130){
+        for x in stride(from: 20, through: 540, by: 40){
+            canvas.lineColor = Color.orange
+            canvas.drawLine(fromX: x, fromY: y, toX: x, toY: z, lineWidth: 10)
+        }
+    }
+    }
+//////    //yellow lines on bottom row
+    for x in stride(from: 30, through: 540, by: 80){
+        canvas.lineColor = Color.yellow
+        canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
+    }
+   //yellow lines on the bottom
+    for x in stride(from: 50, through: 540, by: 80){
+        canvas.lineColor = Color.yellow
+        canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
     }
 }
 
-
-
-canvas.borderColor = Color.white
-canvas.drawRectangle(centreX: 270, centreY: 65, width: 540, height: 0, borderWidth: 55)
-
-canvas.borderColor = Color.white
-canvas.drawRectangle(centreX: 270, centreY: 195, width: 540, height: 0, borderWidth: 55)
-
-canvas.borderColor = Color.white
-canvas.drawRectangle(centreX: 270, centreY: 335, width: 540, height: 0, borderWidth: 55)
-
-canvas.borderColor = Color.white
-canvas.drawRectangle(centreX: 270, centreY: 465, width: 540, height: 0, borderWidth: 55)
-
-for x in stride(from: 10, through: 540, by: 80) {
-canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
-}
-for x in stride(from: 70, through: 540, by: 80){
-    canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
-}
-
-for x in stride(from: 20, through: 540, by: 40){
-    canvas.lineColor = Color.orange
-    canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
-}
-
-for x in stride(from: 30, through: 540, by: 80){
-    canvas.lineColor = Color.yellow
-    canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
-}
-
-for x in stride(from: 50, through: 540, by: 80){
-    canvas.lineColor = Color.yellow
-    canvas.drawLine(fromX: x, fromY: 88, toX: x, toY: 42, lineWidth: 10)
-}
-
-
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
 PlaygroundPage.current.liveView = canvas.imageView
